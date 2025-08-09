3c9c64da4982ea5f0151133748fbccca3ac26ef3
kmcbarne <Keven.McBarnes@gmail.com>
2025-08-08 20:32:41 -0500

    Added and generated documentation

--- Sources/Extensions/String+Extensions.swift
+++ Sources/Extensions/String+Extensions.swift
@@ -11,6 +11,8 @@ import Foundation
 import CommonCrypto
 
 extension String {
+    /// Checks the String and verifies that it is not empty, and that all characters in the String are numbers (0-9).
+    /// - Returns:  A Boolean value signifying that the String contains only numeric characters (0-9).
     var isNumeric: Bool {
         return !(self.isEmpty) && self.allSatisfy { $0.isNumber }
     }
