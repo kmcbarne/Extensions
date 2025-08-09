1402e401d4330b32d06b4ea2e78168cda6362d9b
kmcbarne <Keven.McBarnes@gmail.com>
2025-08-08 20:18:18 -0500

    No changes

--- Sources/Extensions/String+Extensions.swift
+++ Sources/Extensions/String+Extensions.swift
@@ -1,8 +1,18 @@
+//  The Swift Programming Language
+//  https://docs.swift.org/swift-book
 //
-//  File.swift
+//  String+Extensions.swift
 //  Extensions
 //
 //  Created by Keven McBarnes on 8/8/25.
 //
 
 import Foundation
+import CommonCrypto
+
+extension String {
+    var isNumeric: Bool {
+        return !(self.isEmpty) && self.allSatisfy { $0.isNumber }
+    }
+}
+
